404760c119d3aee4104c1d410381df656b8d1f66
Jinli <yu.jinl@husky.neu.edu>
2018-03-14 14:29:43 -0500

    Initial commit

---
+++ app/src/main/java/com/example/jinliyu/testgitpush/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.jinliyu.testgitpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+ private void testupload()
+{
+  //checking if fhile is uploaded to github
+}
+}
